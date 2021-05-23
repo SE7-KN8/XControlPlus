72259fd9a6629df7c64fa21aff1945b1a7096aea
Sebastian Knackstedt <sebknack@gmail.com>
2021-05-23 18:41:16 +0200

    Fix saving and loading.

--- src/main/kotlin/com/github/se7_kn8/xcontrolplus/app/context/ApplicationContext.kt
+++ src/main/kotlin/com/github/se7_kn8/xcontrolplus/app/context/ApplicationContext.kt
@@ -4,6 +4,9 @@ import com.github.se7_kn8.xcontrolplus.app.grid.BaseCell
 import com.github.se7_kn8.xcontrolplus.app.settings.ApplicationSettings
 import com.github.se7_kn8.xcontrolplus.app.settings.UserSettings
 import com.google.gson.*
+import javafx.beans.property.SimpleBooleanProperty
+import javafx.beans.property.SimpleIntegerProperty
+import javafx.beans.property.SimpleStringProperty
 import java.lang.reflect.Type
 
 // From: https://stackoverflow.com/a/9550086/10648509
@@ -26,19 +29,39 @@ class AbstractClassAdapter<T : Any> : JsonSerializer<T>, JsonDeserializer<T> {
 
     private fun typeForName(element: JsonElement): Type {
         try {
-            return Class.forName(this::class.java.`package`.name + "." + element.asString)
+            return Class.forName(BaseCell::class.java.`package`.name + "." + element.asString)
         } catch (e: Exception) {
             throw JsonParseException(e)
         }
     }
+}
 
+class SimpleStringPropertyAdapter : JsonSerializer<SimpleStringProperty>, JsonDeserializer<SimpleStringProperty> {
+    override fun serialize(p0: SimpleStringProperty?, p1: Type?, p2: JsonSerializationContext?) = JsonPrimitive(p0?.get())
+
+    override fun deserialize(p0: JsonElement?, p1: Type?, p2: JsonDeserializationContext?) = SimpleStringProperty(p0?.asString)
+}
+
+class SimpleIntegerPropertyAdapter : JsonSerializer<SimpleIntegerProperty>, JsonDeserializer<SimpleIntegerProperty> {
+    override fun serialize(p0: SimpleIntegerProperty?, p1: Type?, p2: JsonSerializationContext?) = JsonPrimitive(p0?.get())
+
+    override fun deserialize(p0: JsonElement?, p1: Type?, p2: JsonDeserializationContext?) = SimpleIntegerProperty(p0?.asInt!!)
+}
+
+class SimpleBooleanPropertyAdapter : JsonSerializer<SimpleBooleanProperty>, JsonDeserializer<SimpleBooleanProperty> {
+    override fun serialize(p0: SimpleBooleanProperty?, p1: Type?, p2: JsonSerializationContext?) = JsonPrimitive(p0?.get())
+
+    override fun deserialize(p0: JsonElement?, p1: Type?, p2: JsonDeserializationContext?) = SimpleBooleanProperty(p0?.asBoolean!!)
 }
 
 class ApplicationContext() {
 
-    val gson = GsonBuilder()
+    val gson: Gson = GsonBuilder()
         .setPrettyPrinting()
         .registerTypeAdapter(BaseCell::class.java, AbstractClassAdapter<BaseCell>())
+        .registerTypeAdapter(SimpleStringProperty::class.java, SimpleStringPropertyAdapter())
+        .registerTypeAdapter(SimpleIntegerProperty::class.java, SimpleIntegerPropertyAdapter())
+        .registerTypeAdapter(SimpleBooleanProperty::class.java, SimpleBooleanPropertyAdapter())
         .create()
 
     val applicationSettings = ApplicationSettings()
