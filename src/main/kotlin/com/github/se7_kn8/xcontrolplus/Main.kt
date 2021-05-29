63001b558e940f93e76899161f04338ecae9ae5a
Sebastian Knackstedt <sebknack@gmail.com>
2021-05-30 00:52:36 +0200

    Add german translations and possible to change language via system property.

--- src/main/kotlin/com/github/se7_kn8/xcontrolplus/Main.kt
+++ src/main/kotlin/com/github/se7_kn8/xcontrolplus/Main.kt
@@ -2,7 +2,11 @@ package com.github.se7_kn8.xcontrolplus
 
 import com.github.se7_kn8.xcontrolplus.app.XControlPlus
 import javafx.application.Application
+import java.util.*
 
 fun main(args: Array<String>) {
+    if (System.getProperty("lang")?.isNotBlank() == true) {
+        Locale.setDefault(Locale.forLanguageTag(System.getProperty("lang")))
+    }
     Application.launch(XControlPlus::class.java, *args)
 }
